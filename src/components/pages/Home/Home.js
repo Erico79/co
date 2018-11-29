84d752277da2db0f938208f8e268f9a2ace2dad5
Erico79 <emurinyo@gmail.com>
2018-11-29 21:38:59 +0300

    redesign the  landing page  again

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Jumbotron } from "reactstrap";
+import { Jumbotron, Row, Col } from "reactstrap";
 
 import "./Home.css";
 import groupImage from "../../../assets/images/group.svg";
@@ -8,17 +8,31 @@ const home = () => {
   return (
     <div className="Home">
       <Jumbotron className="text-center">
-        <img src={groupImage} height="300" alt="Chama Group" />
-        <h1 className="display-6">Welcome to Chama App</h1>
+        <img src={groupImage} className="mb-3" height="250" alt="Chama Group" />
+        <h3 className="display-6">Welcome to Chama App</h3>
         <p className="lead">This is a platform for Chamas</p>
         <hr className="my-4" />
         <p>Want to Register and move your Chama online?</p>
-        <a className="btn btn-primary btn-lg mr-2" href="/register" role="button">
-          Register your Chama Now!
-        </a>&nbsp;
-        <a className="btn btn-outline-primary btn-lg" href="/register" role="button">
-          Login
-        </a>
+        <Row>
+          <Col md="6" className="mb-2">
+            <a
+              className="btn btn-dark btn-block btn-lg mr-2"
+              href="/register"
+              role="button"
+            >
+              Register your Chama Now!
+            </a>
+          </Col>
+          <Col md="6">
+            <a
+              className="btn btn-outline-dark btn-block btn-lg"
+              href="/register"
+              role="button"
+            >
+              Login
+            </a>
+          </Col>
+        </Row>
       </Jumbotron>
     </div>
   );
